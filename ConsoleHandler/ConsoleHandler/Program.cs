5683e22adb3a8ee543614b4f69b1571cfe2cd772
UncleGus <mr.david.karl@gmail.com>
2015-10-05 21:50:06 +1300

    ConsoleHandler runs in a loop

---
+++ ConsoleHandler/ConsoleHandler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocationHandler;
+
+namespace ConsoleHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            Location Paris = new Location();
+            do
+            {
+                line = Console.ReadLine();
+                Console.WriteLine(line);
+            } while (line != "exit");
+        }
+    }
+}
---
+++ ConsoleHandler/LocationHandler/Location.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocationHandler
+{
+    public class Location
+    {
+        public string name;
+        public LocationType type;
+        public bool isEastern;
+        public Location[] byRoad;
+        public Location[] byTrain;
+        public Location[] bySea;
+
+    }
+
+    public enum LocationType
+    {
+        Town,
+        TownPort,
+        City,
+        CityPort,
+        Sea,
+        Castle,
+        Hospital
+    }
+
+}
